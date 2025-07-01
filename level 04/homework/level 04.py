b0babba8ff314773532f54c8e38118501c5a6dd4
lukamiqutishvili12 <miqutishvililuka27@gmail.com>
2025-07-01 12:17:17 +0400

    second commit

---
+++ level 04/homework/level 04.py
@@ -0,0 +1,11 @@
+#გააკეთე ორი ცვლადი, ორივე ცვლადში ინფუთის მეშვეობით შემოატანინე მომხმარებელს რიცხვი და ბოლოს გამოპრინტე ამ რიცხვების ჯამი
+number1 = int (input("enter your number: "))
+number2 = int (input("enter your number2: "))
+print(number1 + number2)
+
+
+
+
+#მომხმარებელს შემოატანინე რიცხვი და დაპრინტე ეს რიცხვი გაორმაგებული(გაორმაგება: num * 2) 
+number = int(input("enter your number: "))
+print("გაორმაგებული რიცხვი:", number * 2)
